83e6276a849997cb4c6b101ac8fd6c2ed0d94447
Jappie Klooster <jappieklooster@hotmail.com>
2014-02-02 16:49:16 +0100

    copied some code from the internet to open a window... which did not work

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <SDL/SDL.h>
 #include "Grid.h"
 using namespace std;
 namespace isgp{
@@ -14,6 +15,39 @@ namespace isgp{
 	 * open a window
 	 */
 	int main(int argc, char** argv) {
+		SDL_Surface *screen;
+		SDL_Event event;
 
+		if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0)
+		{
+			fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+			exit(1);
+		}
+
+		screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+		if (screen == NULL)
+		{
+			fprintf(stderr, "Unable to set 640x480 video: %s\n", SDL_GetError());
+			exit(1);
+		}
+
+		SDL_WaitEvent(&event);
+
+		while(1)
+		{
+			while(SDL_PollEvent(&event))
+			{     
+				switch (event.type)
+				{
+					case SDL_QUIT:
+						exit(0);
+						break;
+				}
+			}
+		}
+
+		SDL_Quit();
+
+		return 1;
 	}
 }
